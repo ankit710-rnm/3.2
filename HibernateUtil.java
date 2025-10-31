502f0aba776449c27b79cf2d180e7f7449c96bdb
ankit710-rnm <ankitcollege256@gmail.com>
2025-10-31 15:56:21 +0530

    Create HibernateUtil.java

---
+++ HibernateUtil.java
@@ -0,0 +1,20 @@
+// HibernateUtil.java
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+public class HibernateUtil {
+    private static final SessionFactory sessionFactory;
+
+    static {
+        try {
+            sessionFactory = new Configuration().configure() // uses hibernate.cfg.xml
+                    .addAnnotatedClass(Student.class)
+                    .buildSessionFactory();
+        } catch (Throwable ex) {
+            throw new ExceptionInInitializerError(ex);
+        }
+    }
+    public static SessionFactory getSessionFactory() {
+        return sessionFactory;
+    }
+}
